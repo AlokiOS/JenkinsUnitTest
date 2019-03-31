a7af7b9cbca59e5401f5e49ae11af2034769128a
Alok <sinha18alok@gmail.com>
2019-03-31 20:27:52 +0200

    seup

---
+++ JenkinsUnitTest/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  JenkinsUnitTest
+//
+//  Created by Alok Sinha on 2019-03-31.
+//  Copyright © 2019 Alok Sinha. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
