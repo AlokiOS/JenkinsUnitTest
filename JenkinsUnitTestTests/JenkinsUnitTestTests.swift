ee694ba0375277f06d229cff08ef27e2bba959eb
Alok <sinha18alok@gmail.com>
2019-04-01 00:26:31 +0200

    fixed unit test.
    test

--- JenkinsUnitTestTests/JenkinsUnitTestTests.swift
+++ JenkinsUnitTestTests/JenkinsUnitTestTests.swift
@@ -22,7 +22,7 @@ class JenkinsUnitTestTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(true, false)
+        XCTAssertEqual(true, true)
     }
 
     func testPerformanceExample() {
